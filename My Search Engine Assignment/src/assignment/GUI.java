feab635c19cd93ebd0c07572930f91e8426b4c3e
RichardI45 <102364933+RichardI45@users.noreply.github.com>
2022-04-04 00:59:53 +0100

    Created Search Class
    Cleaned everything up

--- My Search Engine Assignment/src/assignment/GUI.java
+++ My Search Engine Assignment/src/assignment/GUI.java
@@ -1,10 +1,6 @@
 package assignment;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+
 
 public class GUI {
 
--- My Search Engine Assignment/src/assignment/Window.java
+++ My Search Engine Assignment/src/assignment/Window.java
@@ -1,7 +1,6 @@
 package assignment;
 
 import java.awt.FlowLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
