cab8ef401f2ea8318cb7897340fefd1729be139f
RichardI45 <102364933+RichardI45@users.noreply.github.com>
2022-04-03 16:22:34 +0100

    First Frame and Panel

---
+++ My Search Engine Assignment/src/assignment/GUI.java
@@ -0,0 +1,33 @@
+package assignment;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GUI {
+
+	public static void main(String[] args) {
+		
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		
+		panel.setLayout(null);
+		
+		JLabel label = new JLabel("Enter Word");
+		label.setBounds(10,20,80,25);
+		panel.add(label);
+		
+		JTextField userText = new JTextField(20);
+		userText.setBounds(100,20,165,25);
+		panel.add(userText);
+		
+		
+		frame.setVisible(true);
+		
+	}
+
+}
