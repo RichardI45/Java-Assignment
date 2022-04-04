cd0a958e7a57ff3b31c1e9abc1f55473e538b69d
RichardI45 <102364933+RichardI45@users.noreply.github.com>
2022-04-04 14:55:04 +0100

    GUI
    added  more buttons and fixed the gui screen

--- My Search Engine Assignment/src/assignment/GUI.java
+++ My Search Engine Assignment/src/assignment/GUI.java
@@ -5,29 +5,7 @@ package assignment;
 public class GUI {
 
 	public static void main(String[] args) {
-		/*
-		JPanel panel = new JPanel();
-		JFrame frame = new JFrame();
-		frame.setSize(500,500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(panel);
 		
-		panel.setLayout(null);
-		
-		JLabel label = new JLabel("Enter Word");
-		label.setBounds(10,20,80,25);
-		panel.add(label);
-		
-		JTextField userText = new JTextField(20);
-		userText.setBounds(100,20,165,25);
-		panel.add(userText);
-		
-		JButton button = new JButton("Search");
-		button.setBounds(10,110,300,25);
-		panel.add(button);
-		
-		frame.setVisible(true);
-		*/
 		
 		Window mywindow = new Window();
 		
--- My Search Engine Assignment/src/assignment/Window.java
+++ My Search Engine Assignment/src/assignment/Window.java
@@ -1,35 +1,69 @@
 package assignment;
 
-import java.awt.FlowLayout;
+
+
+
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
 
 public class Window extends JFrame{
 	
+	
+	
 	Window(){
+		
+	
 	
 	this.setTitle("My Search Engine");
 	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	this.setSize(400,400);
-	this.setLayout(new FlowLayout(FlowLayout.LEFT));
+	this.setLayout(null);
+	
+	JPanel panel = new JPanel();
+	this.add(panel);
+	
+	JLabel Title = new JLabel("Find Word");
+	Title.setBounds(140,25,300,50);
+	Title.setFont(new Font("Calibri",Font.BOLD,20));
+	this.add(Title);
 	
 	JLabel label = new JLabel("Enter Word");
-	label.setBounds(10,20,80,50);
+	label.setBounds(10,65,80,50);
 	this.add(label);
 	
-	JTextField userText = new JTextField(20);
-	userText.setBounds(100,20,165,50);
-	this.add(userText);
+	JTextField searchBox = new JTextField(20);
+	searchBox.setBounds(100,80,165,25);
+	this.add(searchBox);
 	
-	JButton button = new JButton("Search");
-	button.setBounds(10,110,300,50);
-	this.add(button);
+	JButton searchButton = new JButton("Search");
+	searchButton.setBounds(100,140,150,25);
+	this.add(searchButton);
+	
+	JButton addFileButton = new JButton("Add File");
+	addFileButton.setBounds(70,110,110,25);
+	this.add(addFileButton);
+	
+	JButton clearFileButton = new JButton("Clear File");
+	clearFileButton.setBounds(180,110,110,25);
+	this.add(clearFileButton);
+	
 	
 	
 	this.setVisible(true);
+	
 	}
-
 }
+				
+							
+						
+					
+	
+
+
